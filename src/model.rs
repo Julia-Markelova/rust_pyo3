c71dae6bb8a156d27b6bfe996ec5bc721b7badc7
Yulia Markelova <markelova.y@digdes.com>
2021-05-11 15:06:17 +0300

    add getters

--- src/model.rs
+++ src/model.rs
@@ -38,6 +38,16 @@ impl Rectangle {
     fn new(width_m: f64, length_m: f64) -> Self {
         Rectangle { width_m, length_m }
     }
+
+    #[getter]
+    fn width_m(&self) -> PyResult<f64> {
+        Ok(self.width_m)
+    }
+
+    #[getter]
+    fn length_m(&self) -> PyResult<f64> {
+        Ok(self.length_m)
+    }
 }
 
 
@@ -46,6 +56,21 @@ impl Position {
     #[new]
     fn new(offset_x_m: f64, offset_y_m: f64, angle_deg: f64) -> Self {
         Position { offset_x_m, offset_y_m, angle_deg }
+    }
+
+    #[getter]
+    fn offset_x_m(&self) -> PyResult<f64> {
+        Ok(self.offset_x_m)
+    }
+
+    #[getter]
+    fn offset_y_m(&self) -> PyResult<f64> {
+        Ok(self.offset_y_m)
+    }
+
+    #[getter]
+    fn angle_deg(&self) -> PyResult<f64> {
+        Ok(self.angle_deg)
     }
 }
 
@@ -56,6 +81,16 @@ impl Building {
     fn new(rectangle: Rectangle, position: Position) -> Self {
         Building { rectangle, position }
     }
+
+    #[getter]
+    fn rectangle(&self) -> PyResult<Rectangle> {
+        Ok(self.rectangle)
+    }
+
+    #[getter]
+    fn position(&self) -> PyResult<Position> {
+        Ok(self.position)
+    }
 }
 
 
@@ -65,4 +100,10 @@ impl Cluster {
     fn new(buildings: Vec<Building>, position: Position) -> Self {
         Cluster { buildings, position }
     }
+
+    #[getter]
+    fn position(&self) -> PyResult<Position> {
+        Ok(self.position)
+    }
+
 }
